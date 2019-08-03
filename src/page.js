73186915126ba6946bf1c835ab65a55e433b3681
Denis <dev@dbribe.com>
2019-08-03 13:33:40 +0300

    * Added support for “foodpanda”

--- src/page.js
+++ src/page.js
@@ -502,9 +502,73 @@
     return TakeAway;
   }(BasePlatform);
 
+  var FoodPanda =
+  /*#__PURE__*/
+  function (_BasePlatform) {
+    _inherits(FoodPanda, _BasePlatform);
+
+    function FoodPanda() {
+      _classCallCheck(this, FoodPanda);
+
+      return _possibleConstructorReturn(this, _getPrototypeOf(FoodPanda).apply(this, arguments));
+    }
+
+    _createClass(FoodPanda, [{
+      key: "getDishes",
+      value: function getDishes() {
+        var elements = document.querySelectorAll(".dish-card");
+        return _toConsumableArray(elements).map(function (element) {
+          return {
+            element: element,
+            title: element.querySelector(".dish-name").innerText.trim().toLowerCase(),
+            price: parseFloat(element.querySelector('.price').innerText.replace(",", "."))
+          };
+        });
+      }
+    }, {
+      key: "orderItems",
+      value: function orderItems(itemsToOrder) {
+        var index = -1;
+        var total = 0;
+        var intervalOrder = setInterval(function () {
+          index++;
+
+          if (index == itemsToOrder.length) {
+            clearInterval(intervalOrder);
+          }
+
+          var item = itemsToOrder[index];
+          item.element.click();
+          total += item.price;
+          console.log(total);
+          setTimeout(function () {
+            if (parseFloat(document.querySelector(".desktop-cart__order__total-price").innerText.replace(",", ".")) != total.toFixed(2)) {
+              item.element.parentNode.querySelector(".cartbutton-button-sidedishes").click();
+            }
+          }, 1400);
+        }, 2000);
+      }
+    }, {
+      key: "fillOrder",
+      value: function fillOrder() {
+        document.querySelector("#iaddress").value = config.street + config.number;
+        document.querySelector("#iblock").value = config.block;
+        document.querySelector("#ientrance").value = config.entrance;
+        document.querySelector("#ifloor").value = config.floor;
+        document.querySelector("#iapartmentname").value = config.apt;
+        document.querySelector("#iintercom").value = config.intercom;
+        document.querySelector("#isurname").value = config.firstName + config.lastName;
+        document.querySelector("#iphonenumber").value = config.phone;
+        document.querySelector("#ipaymentmethods > div.paymentbuttonwrapper.payment-method-type-online.paymentmethod31_2.paymentbuttonchecked").click();
+      }
+    }]);
+
+    return FoodPanda;
+  }(BasePlatform);
+
   window.addEventListener("foodMessage", function (event) {
     var getMerchantOrder = function getMerchantOrder() {
-      return window.location.host === "www.caserola.ro" ? Caserola : window.location.host === "sushi-terra.ro" ? SushiTerra : TakeAway;
+      return window.location.host === "www.caserola.ro" ? Caserola : window.location.host === "sushi-terra.ro" ? SushiTerra : window.location.host === "www.foodpanda.ro" ? FoodPanda : TakeAway;
     };
 
     event = event.detail;
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,13 +1,15 @@
 import {Caserola} from "./platforms/Caserola";
 import {SushiTerra} from "./platforms/SushiTerra";
 import {TakeAway} from "./platforms/TakeAway";
+import {FoodPanda} from "./platforms/FoodPanda";
 
 
 
 window.addEventListener("foodMessage", event => {
     const getMerchantOrder = () => {
         return window.location.host === "www.caserola.ro" ? Caserola :
-            window.location.host === "sushi-terra.ro" ? SushiTerra : TakeAway;
+            window.location.host === "sushi-terra.ro" ? SushiTerra :
+            window.location.host === "www.foodpanda.ro" ? FoodPanda : TakeAway;
     };
 
     event = event.detail;
