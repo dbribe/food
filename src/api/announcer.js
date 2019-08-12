1ab5e2b8ec45c73c3188fece433a0509a46ffbbd
Denis <dev@dbribe.com>
2019-08-12 10:32:38 +0300

    * Be case agnostic

--- src/api/announcer.js
+++ src/api/announcer.js
@@ -1,7 +1,7 @@
 const {google} = require('googleapis');
 const {PLACES, PEOPLE_COUNT, ABSENT_PEOPLE, sheetsLink, sheetsId, slackHook, slackUsers} = require('./constants.js');
 
-const place = PLACES[0];
+const place = PLACES[3];
 
 const Slack = require('node-slack');
 const slack = new Slack(slackHook,{});
@@ -15,10 +15,10 @@ const announce = (auth) => {
     }, (err, res) => {
         const rows = res.data.values;
         console.log(rows[rows.length - 1]);
-        let message = "Salut. Maine o sa comandam de la <" + rows[rows.length - 1][1] + "|" + place + ">.";
+        let message = "Salut. Azi o sa comandam de la <" + rows[rows.length - 1][1] + "|" + place + ">.";
         message += "\nDaca doriti sa va schimbati defaulturile intrati <" + sheetsLink + "|aici>.\nComanda este urmatoarea:";
         for (const row of rows) {
-            if (!row[0] || ABSENT_PEOPLE.find(r => r === row[0])) {
+            if (!row[0] || ABSENT_PEOPLE.find(r => r.toLowerCase() === row[0].toLowerCase())) {
                 continue;
             }
             message += "\n<@" + slackUsers[row[0].toLowerCase()] + "> " + (row[1] || "");
