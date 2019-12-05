1205f0406cf77ce35351857f4fe19b42fc479688
Denis <dev@dbribe.com>
2019-12-05 09:08:25 +0200

    Improved UI UX

--- src/api/announcer.js
+++ src/api/announcer.js
@@ -1,32 +1,31 @@
 const {google} = require('googleapis');
-const {PLACES, PEOPLE_COUNT, ABSENT_PEOPLE, sheetsLink, sheetsId, slackHook, slackUsers} = require('./constants.js');
-
-const place = PLACES[3];
+const {PEOPLE_COUNT, PLACE, sheetsLink, sheetsId, slackHook, slackUsers} = require('./constants.js');
 
 const Slack = require('node-slack');
 const slack = new Slack(slackHook,{});
 
-
 const announce = (auth) => {
+    const absentPeople = process.argv.slice(2);
+
     const sheets = google.sheets({version: 'v4', auth});
     sheets.spreadsheets.values.get({
         spreadsheetId: sheetsId,
-        range: place + "!A2:B" + (PEOPLE_COUNT + 2),
+        range: PLACE + "!A2:B" + (PEOPLE_COUNT + 2),
     }, (err, res) => {
         const rows = res.data.values;
         console.log(rows[rows.length - 1]);
-        let message = "Salut. Azi o sa comandam de la <" + rows[rows.length - 1][1] + "|" + place + ">.";
+        let message = "Salut. Azi o sa comandam de la <" + rows[rows.length - 1][1] + "|" + PLACE + ">.";
         message += "\nDaca doriti sa va schimbati defaulturile intrati <" + sheetsLink + "|aici>.\nComanda este urmatoarea:";
         for (const row of rows) {
-            if (!row[0] || ABSENT_PEOPLE.find(r => r.toLowerCase() === row[0].toLowerCase())) {
+            if (!row[0] || absentPeople.find(r => row[0].toLowerCase().match(r.toLowerCase()))) {
                 continue;
             }
             message += "\n<@" + slackUsers[row[0].toLowerCase()] + "> " + (row[1] || "");
         }
-        slack.send({
-            text: message,
-            channel: '#food',
-        });
+        // slack.send({
+        //     text: message,
+        //     channel: '#food',
+        // });
 
         console.log(message);
     });
